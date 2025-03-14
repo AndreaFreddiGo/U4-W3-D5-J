594e6e09daed518f057cc4e65690ea63232b707d
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2025-03-14 12:47:46 +0100

    Initial commit

---
+++ src/test/java/andrea_freddi/ApplicationTest.java
@@ -0,0 +1,13 @@
+package andrea_freddi;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
