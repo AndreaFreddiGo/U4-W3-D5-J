89570e638540df68134111e2356708cee105ef0d
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2025-03-14 19:39:30 +0100

    Adds new objects

--- src/main/java/andrea_freddi/Application.java
+++ src/main/java/andrea_freddi/Application.java
@@ -12,6 +12,10 @@ import com.github.javafaker.Faker;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.Random;
 
 public class Application {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("U4-W3-D5-J");
@@ -22,25 +26,27 @@ public class Application {
         ElementiDAO elementiDAO = new ElementiDAO(em);
         UtentiDAO utentiDAO = new UtentiDAO(em);
         PrestitiDAO prestitiDAO = new PrestitiDAO(em);
+        Random random = new Random();
 
         // creo elementi del catalogo e li salvo nel database
         for (int i = 0; i < 10; i++) {
             Libro libro = new Libro(faker.number().numberBetween(1950, 2025), faker.code().isbn10(), faker.number().numberBetween(100, 400), faker.book().title(), faker.book().author(), faker.book().genre());
-            elementiDAO.save(libro);
+//            elementiDAO.save(libro);
         }
 
         Rivista rivista1 = new Rivista(faker.number().numberBetween(2015, 2025), faker.code().isbn10(), faker.number().numberBetween(20, 50), faker.book().genre() + " Magazine", Periodicità.MENSILE);
-        elementiDAO.save(rivista1);
         Rivista rivista2 = new Rivista(faker.number().numberBetween(2015, 2025), faker.code().isbn10(), faker.number().numberBetween(20, 50), faker.book().genre() + " Magazine", Periodicità.SETTIMANALE);
-        elementiDAO.save(rivista2);
         Rivista rivista3 = new Rivista(faker.number().numberBetween(2015, 2025), faker.code().isbn10(), faker.number().numberBetween(20, 50), faker.book().genre() + " Magazine", Periodicità.SEMESTRALE);
-        elementiDAO.save(rivista3);
         Rivista rivista4 = new Rivista(faker.number().numberBetween(2015, 2025), faker.code().isbn10(), faker.number().numberBetween(20, 50), faker.book().genre() + " Magazine", Periodicità.MENSILE);
-        elementiDAO.save(rivista4);
         Rivista rivista5 = new Rivista(faker.number().numberBetween(2015, 2025), faker.code().isbn10(), faker.number().numberBetween(20, 50), faker.book().genre() + " Magazine", Periodicità.SETTIMANALE);
-        elementiDAO.save(rivista5);
         Rivista rivista6 = new Rivista(faker.number().numberBetween(2015, 2025), faker.code().isbn10(), faker.number().numberBetween(20, 50), faker.book().genre() + " Magazine", Periodicità.SEMESTRALE);
-        elementiDAO.save(rivista6);
+
+//        elementiDAO.save(rivista1);
+//        elementiDAO.save(rivista2);
+//        elementiDAO.save(rivista3);
+//        elementiDAO.save(rivista4);
+//        elementiDAO.save(rivista5);
+//        elementiDAO.save(rivista6);
 
         // creo utenti e li salvo nel database
         for (int i = 0; i < 10; i++) {
@@ -49,6 +55,13 @@ public class Application {
         }
 
         // creo prestiti e li salvo nel database
+        Date startDate = Date.from(LocalDate.of(2025, 1, 1).atStartOfDay(ZoneId.systemDefault()).toInstant());
+        Date endDate = Date.from(LocalDate.of(2025, 03, 01).atStartOfDay(ZoneId.systemDefault()).toInstant());
+        for (int i = 0; i < 16; i++) {
+            elementiDAO.findById(i);
+            utentiDAO.findById(random.nextInt(10));
+            prestitiDAO.save(faker.date().between(startDate, endDate), );
+        }
 
 
         em.close();
